1b4c7997f308d449bb7e6565662b2133149d0090
gonza <gonzalo.christobal@gmail.com>
2019-05-30 23:33:51 -0400

    Firebase config and context created

---
+++ src/components/Firebase.js
@@ -0,0 +1,21 @@
+import React, { createContext } from "react";
+import firebase from "firebase";
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
+};
+
+export const FirebaseContext = createContext(null);
+export const FirebaseProvider = ({ children }) => {
+  return (
+    <FirebaseContext.Provider value={firebase.initializeApp(config)}>
+      {children}
+    </FirebaseContext.Provider>
+  );
+};
