50e5caeee4cf5ae51f67f14634086a84f81ae2d0
gonza <gonzalo.christobal@gmail.com>
2019-05-30 23:34:03 -0400

    Slug updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Events from './Events';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Events from "./Events";
+import { FirebaseProvider } from "./Firebase";
 
 const App = () => {
   return (
     <Router>
-      <Route exact path="/" component={Events} />
+      <FirebaseProvider>
+        <Route exact path="/:location/:date" component={Events} />
+      </FirebaseProvider>
     </Router>
   );
-}
+};
 
 export default App;
